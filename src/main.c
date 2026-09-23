f499d44b5817e0ac33e090daf56e0b8f50076813
Jim Marshall <jim.marshall@codethink.co.uk>
2026-09-23 20:28:52 +0100

    Core engine stuff added

--- src/main.c
+++ src/main.c
@@ -1,1 +1,15 @@
 #include "cglm/cglm.h"
+#include "Log.h"
+
+int main(int argc, const char** argv)
+{
+    struct LogArgs args =
+    {
+        .bIncludeLogTimeStamps = true,
+        .bLogTextColoured = true,
+        .bLogToConsole = true,
+        .logfilePath = NULL
+    };
+    Log_Init(&args);
+    
+}
